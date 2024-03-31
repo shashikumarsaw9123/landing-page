a2cb675d3b65831f80df6e087a2c776c8084d30c
shashikumarsaw9123 <160309187+shashikumarsaw9123@users.noreply.github.com>
2024-03-31 11:11:56 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+let a=document.getElementById("c")
+let b=document.getElementById("d")
+let c=document.getElementById("e")
+let d=document.getElementById("f")
+let e=document.getElementById("g")
+let f=document.getElementById("con1")
+let g=document.getElementById("cno2")
+let h=document.getElementById("con3")
+let i=document.getElementById("con4")
+let j=document.getElementById("con7")
+a.addEventListener("click",()=>{
+    let temp=document.getElementById("con1")
+    temp.scrollIntoView({
+        behavior:"smooth"
+    })
+
+})
+b.addEventListener("click",()=>{
+    let temp=document.getElementById("con3")
+    temp.scrollIntoView({
+        behavior:"smooth"
+    })
+
+})
+c.addEventListener("click",()=>{
+    let temp=document.getElementById("con4")
+    temp.scrollIntoView({
+        behavior:"smooth"
+    })
+
+})
+
+
+d.addEventListener("click",()=>{
+    let temp=document.getElementById("con7")
+    temp.scrollIntoView({
+        behavior:"smooth"
+    })
+
+})
+e.addEventListener("click",()=>{
+    let temp=document.getElementById("con6")
+    temp.scrollIntoView({
+        behavior:"smooth"
+    })
+
+})
+    
+    
+        
+    
